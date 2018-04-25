91be172a4a268cb146e8c051c4d3fffde766c153
sheepQAQ <yangflo@foxmail.com>
2018-04-25 19:39:21 +0800

    微调，不影响页面和逻辑

--- js/card.js
+++ js/card.js
@@ -6,60 +6,58 @@
 
 // 日期格式化，支持输入参数为number和date对象
 function formatDate(date) {
-    var temp;
-    if(typeof date == "number"){
-        temp = new Date(date);
-    }else{
-        temp = date;
-    }
-    var str = temp.getFullYear()+'-'+temp.getMonth()+1+'-'+temp.getDate()+'0 '+temp.getHours()+':'+temp.getMinutes()+':'+temp.getSeconds();
-    return str;
+	var temp;
+	if (typeof date == "number") {
+		temp = new Date(date);
+	} else {
+		temp = date;
+	}
+	var str = temp.getFullYear() + '-' + temp.getMonth() + 1 + '-' + temp.getDate() + '0 ' + temp.getHours() + ':' + temp.getMinutes() + ':' + temp.getSeconds();
+	return str;
 }
 
 // 构造一个账单
 function buildBill(ele, data) {
-    // 创建一个文档片段和临时容器，减小添加元素的回流
-    var fragment = document.createDocumentFragment(),tempdiv = document.createElement("div");
-    for (var i = 0,length = data.length; i < length; i++) {
-        var temp = '<li class="d-item">\n' +
-            '                    <p class="desc">'+ data[i].desc+'</p>\n' +
-            '                    <p class="time">'+ formatDate(data[i].time) +'</p>\n' +
-            '                    <p class="amount">'+ data[i].amount +'</p>\n' +
-            '                </li>';
-        tempdiv.innerHTML = temp;
-        fragment.appendChild(tempdiv.firstChild);
-    }
-    ele.appendChild(fragment);
-    fragment = null;
+	// 创建一个文档片段和临时容器，减小添加元素的回流
+	var fragment = document.createDocumentFragment(),
+		tempdiv = document.createElement("div");
+	for (var i = 0, length = data.length; i < length; i++) {
+		var temp = '<li class="d-item">\n' +
+			'                    <p class="desc">' + data[i].desc + '</p>\n' +
+			'                    <p class="time">' + formatDate(data[i].time) + '</p>\n' +
+			'                    <p class="amount">' + data[i].amount + '</p>\n' +
+			'                </li>';
+		tempdiv.innerHTML = temp;
+		fragment.appendChild(tempdiv.firstChild);
+	}
+	ele.appendChild(fragment);
+	fragment = null;
 
 }
 
-window.onload = function(){
-    // 返回主菜单
-    var backBtn = document.getElementById('g-back');
-    backBtn.addEventListener('click',function (e) {
-        e.preventDefault();
-        window.history.back(-1);
-    });
+window.onload = function () {
+	// 返回主菜单
+	var backBtn = document.getElementById('g-back');
+	backBtn.addEventListener('click', function (e) {
+		e.preventDefault();
+		window.history.back(-1);
+	});
 
-    // 缓存节点
-    var balance = document.querySelector('.result>.info>.balance>span');
-    var detail = document.getElementsByClassName('detail')[0];
-    // 请求获得一卡通数据
-    var token = getCookie()["herald-gusu-token"];
-    axios.get("https://myseu.cn/ws3/api/card",{
-        headers: {
-            'token':token
-        }
-    })
-        .then(function (response) {
-            balance.innerHTML = response.data.result.info.balance;
-            buildBill(detail,response.data.result.detail);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+	// 缓存节点
+	var balance = document.querySelector('.result>.info>.balance>span');
+	var detail = document.getElementsByClassName('detail')[0];
+	// 请求获得一卡通数据
+	var token = getCookie()["herald-gusu-token"];
+	axios.get("https://myseu.cn/ws3/api/card", {
+			headers: {
+				'token': token
+			}
+		})
+		.then(function (response) {
+			balance.innerHTML = response.data.result.info.balance;
+			buildBill(detail, response.data.result.detail);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
 };
-
-
-
--- js/index.js
+++ js/index.js
@@ -10,74 +10,73 @@
  */
 window.onload = function () {
 
-    // 判断登录状态
-    var cookie = getCookie() || {};
-    var token = cookie["herald-gusu-token"];
-    if (token == null) {
-        document.getElementsByClassName('login')[0].classList.remove('none');
-    } else {
-        document.getElementsByClassName('menu')[0].classList.remove('none');
-    }
+	// 判断登录状态
+	var cookie = getCookie() || {};
+	var token = cookie["herald-gusu-token"];
+	if (token == null) {
+		document.getElementsByClassName('login')[0].classList.remove('none');
+	} else {
+		document.getElementsByClassName('menu')[0].classList.remove('none');
+	}
 
-    // 登录事件注册
-    var loginBtn = document.getElementById('loginBtn');
-    loginBtn.addEventListener('click', function (event) {
-        event.preventDefault();
-        // 获取表单数据
-        let cardnum = document.getElementById('cardnum').value;
-        let psd = document.getElementById('password').value;
-        let gpsd = document.getElementById('gpassword').value;
+	// 登录事件注册
+	var loginBtn = document.getElementById('loginBtn');
+	loginBtn.addEventListener('click', function (event) {
+		event.preventDefault();
+		// 获取表单数据
+		let cardnum = document.getElementById('cardnum').value;
+		let psd = document.getElementById('password').value;
+		let gpsd = document.getElementById('gpassword').value;
 
-        // 发送请求 platform 是研究生会登录的默认值
-        axios.post('https://myseu.cn/ws3/auth', {
-                'cardnum': cardnum,
-                'password': psd,
-                'gpassword': gpsd,
-                'platform': 'gusu'
-            })
-            .then(function (response) {
-                token = response.data.result;
-                console.log(token);
-                setCookie('herald-gusu-token', token, 30);
-                location.reload();
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    });
+		// 发送请求 platform 是研究生会登录的默认值
+		axios.post('https://myseu.cn/ws3/auth', {
+				'cardnum': cardnum,
+				'password': psd,
+				'gpassword': gpsd,
+				'platform': 'gusu'
+			})
+			.then(function (response) {
+				token = response.data.result;
+				console.log(token);
+				setCookie('herald-gusu-token', token, 30);
+				location.reload();
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	});
 
-    // 注销事件注册
-    var quitBtn = document.getElementById('g-quit');
-    quitBtn.addEventListener('click', function (event) {
-        event.preventDefault();
-        clearCookie("herald-gusu-token");
-        location.reload();
-    });
+	// 注销事件注册
+	var quitBtn = document.getElementById('g-quit');
+	quitBtn.addEventListener('click', function (event) {
+		event.preventDefault();
+		clearCookie("herald-gusu-token");
+		location.reload();
+	});
 
-    // 跳转功能入口
-    var funcs = document.getElementsByClassName('m-item');
-    for (var i = 0, length = funcs.length; i < length; i++) {
-        (function (i) {
-            funcs[i].addEventListener('click', function (event) {
-                event.preventDefault();
-                switch (i) {
-                    case 0:
-                        // 跳转到 curriculum
-                        window.location.href="page/curriculum.html";
-                        break;
-                    case 1:
-                        //跳转到 gpa
-                        window.location.href="page/gpa.html";
-                        break;
-                    case 2:
-                        // 跳转到 card
-                        window.location.href="page/card.html";
-                        break;
-                    default:
-                        break;
-                }
-            });
-        })(i);
-
-    }
+	// 跳转功能入口
+	var funcs = document.getElementsByClassName('m-item');
+	for (var i = 0, length = funcs.length; i < length; i++) {
+		(function (i) {
+			funcs[i].addEventListener('click', function (event) {
+				event.preventDefault();
+				switch (i) {
+					case 0:
+						// 跳转到 curriculum
+						window.location.href = "page/curriculum.html";
+						break;
+					case 1:
+						//跳转到 gpa
+						window.location.href = "page/gpa.html";
+						break;
+					case 2:
+						// 跳转到 card
+						window.location.href = "page/card.html";
+						break;
+					default:
+						break;
+				}
+			});
+		})(i);
+	}
 };
